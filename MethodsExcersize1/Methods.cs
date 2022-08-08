db267b27368d7710dba6c3eba4423962afc38276
Cody <Cojo341@gmail.com>
2022-08-08 12:21:53 -0400

    inital commit

---
+++ MethodsExcersize1/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsExcersize1
+{
+    internal class Methods
+    {
+        public static void Name()
+            {
+            Console.WriteLine("Hello What is your name?");
+            }
+        
+        public static string ResultSentence(string firstname, string fruit, int amount, string classmate, int amount2  )
+        {
+            return $"{firstname} picked {amount} of {fruit}'s and gave {amount2} of them to {classmate}";       
+        }
+         
+    }
+}
---
+++ MethodsExcersize1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodsExcersize1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Methods.Name();
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Name a Fruit");
+            
+             string color = Console.ReadLine();
+
+            Console.WriteLine("Please pick a number");
+             var amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please pick another number");
+             var amount2 = int.Parse(Console.ReadLine());
+
+            while (amount2 > amount)
+            {
+                Console.WriteLine($"Please Pick a number below {amount}.");
+                 amount2 = int.Parse(Console.ReadLine());
+            }
+           
+            Console.WriteLine("Pick one of your classmates and write their name");
+            string classmate = Console.ReadLine();
+
+            Console.WriteLine(Methods.ResultSentence(firstName, color , amount, classmate, amount2));
+
+        }
+    }
+}
